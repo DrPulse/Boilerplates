f6f070c9673877535aae6888d1c172b19d4f8795
DrPulse <ImpulseG@pm.me>
2023-10-28 21:41:44 +0200

    refactor: add details when missing python version or missing poetry binary

--- cookiecutter/project_CLI/hooks/post_gen_project.py
+++ cookiecutter/project_CLI/hooks/post_gen_project.py
@@ -5,6 +5,8 @@ def run_poetry_install():
         subprocess.run(["poetry", "install"], check=True)
         print("Command 'poetry install' completed successfully.")
     except subprocess.CalledProcessError as e:
-        print(f"Error running 'poetry install': {e}")
+        print(f"Error running 'poetry install'. Check that the specified python version is installed. Error Details:\n{e}")
+    except FileNotFoundError as e:
+        print(f"Poetry not installed, can't install dependencies. {e}")
 
 run_poetry_install()
